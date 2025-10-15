b5bef62c12ef06fe5deeb05bfec96ecce8881025
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-15 10:25:30 +0530

    solution 5

---
+++ scripts/script5.js
@@ -0,0 +1,4 @@
+"when we have init() and setupEvents() functions:"
+
+"we should declare the event functions in setupEvents() function"
+"then the functions which was called on event should be declared in init() function"
