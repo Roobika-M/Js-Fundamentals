df1e8b66be0ef5a7a474d3b8b74763cd7869630b
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-15 09:57:35 +0530

    solution 3

---
+++ scripts/script3.js
@@ -0,0 +1,5 @@
+alert("hello all");
+
+"when the script tag is placed before the body tag, the content will be loaded before the html body content is rendered, so it won't execute"
+
+"when the script tag is placed after body tag, the script won't be executed"
