3a1dcf731ae3b5a5bc1f3441c37ad28eb83d7509
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 19:22:44 +0530

    solution 26

---
+++ scripts/script26.js
@@ -0,0 +1,8 @@
+let num = 5;
+let str = "5";
+
+console.log(num == str);  // Outputs: true 
+console.log(num === str); // Outputs: false 
+
+//  == checks only the value and not datatype
+// === checks both value and datatype
