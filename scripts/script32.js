debd88ba05aa84326c9fdbc5aba86df487944a0d
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 21:12:42 +0530

    solution 32

---
+++ scripts/script32.js
@@ -0,0 +1,9 @@
+let a = true;
+let b = true;
+let c = false;
+
+console.log(a && b && c);  // false
+console.log(a || b || c);  // true
+
+// for AND operator, all conditions should be true to return true
+// for OR operator, at least one condition should be true to return true
