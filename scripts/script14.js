c97892e1099a30f68e9134043f7629da811f113d
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 14:48:38 +0530

    solution 14

---
+++ scripts/script14.js
@@ -0,0 +1,4 @@
+// difference between null and undefined
+
+// when we assign a value "null" to a variable, we are purposely setting the value as null to the variable. 
+// whereas in case of undefined, we didn't assign any value to the variable.
