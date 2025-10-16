7fdbf5f4f4c83b83f61fad6a73d570eb80320681
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 22:16:16 +0530

    solution 37

---
+++ scripts/script37.js
@@ -0,0 +1,9 @@
+//  whenever we use ?? with ||, usage of parantheses is a must
+
+let a = null;
+let b = "hi";
+let c = "Hello";
+
+let result = a ?? b || c; // error without parantheses
+let result = a ?? (b || c); // correct usage with parantheses
+console.log(result); // hi
