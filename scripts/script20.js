edec4783c774f85804772658a80420128de74c1f
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 17:27:35 +0530

    solution 20

---
+++ scripts/script20.js
@@ -0,0 +1,14 @@
+//  + operator
+// + is used for string concatenation, so if one value is string and other one is number, it'll be converted as string
+
+let str1 = "" + 1 // output = "1"
+let str2 = "hello" + 1 // output = "hello1"
+let str3 = true + "1" // output = "true1"
+
+// - , *, / operator
+// these values works on numbers, so it'll be converted to numbers
+
+let num1 = "5" - 1 // output = 4
+let num2 = "5" * 2 // output = 10
+let num3 = "5" / 2 // output = 2.5
+let num4 = "" -  1 // output = -1
