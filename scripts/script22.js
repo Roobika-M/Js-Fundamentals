d790fa8ab85faf3f2680a3efbc6796cc65045753
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 17:38:43 +0530

    solution 22

---
+++ scripts/script22.js
@@ -0,0 +1,11 @@
+Boolean("0");
+// returns true because "0" is a non-empty string
+
+Boolean("");
+// returns false because "" is a empty string
+
+Boolean(0);
+// returns false because 0 is falsy value
+
+Boolean(null);
+// returns false because null is falsy value
