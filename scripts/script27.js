f52ba492969d2368ab0774fa54997ef5dbdfa4d8
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 20:17:16 +0530

    solution 27

---
+++ scripts/script27.js
@@ -0,0 +1,5 @@
+console.log(null == undefined); // true
+// both null and undefined represent absence of value
+
+console.log(null === undefined); // false
+// null is an object type, whereas undefined is noted as "undefined" as its type. since the types are dissimilar, the output is false
