5c3cfe262f32c03594dbaaaf3f387dd0fe92752f
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 12:21:25 +0530

    solution 7

---
+++ scripts/script7.js
@@ -0,0 +1,11 @@
+// // without 'use strict'
+x = 5;
+alert(x);
+
+// with 'use strict'
+'use strict';
+y = 5;
+alert(y);
+
+//when I didnt use the 'use strict' at the top of the code and when i declared a variable without a datatype, it is assigned as a global variable.
+//But when I used the 'use strict' at the top of the code and when i declared a variable without a datatype, it gives an error that the variable is not defined. "Uncaught ReferenceError: y is not defined"
