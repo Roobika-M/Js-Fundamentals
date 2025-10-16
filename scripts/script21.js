451255644a2940588d712db915f09e79feb2b1f0
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 17:35:30 +0530

    solution 21

---
+++ scripts/script21.js
@@ -0,0 +1,9 @@
+// Not A Number
+
+let num1 = "hello" - 1; // output = NaN
+console.log(typeof num1); // number
+
+let num2 = undefined + 1; // output = NaN
+console.log(typeof num2); // number
+
+// the typeof NaN is number
