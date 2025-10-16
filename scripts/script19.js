e6ee7b88bcacb11ba3e9ec7fa46c55bfb11a7057
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 17:16:31 +0530

    solution 19

---
+++ scripts/script19.js
@@ -0,0 +1,14 @@
+// string to number
+let num1 = "55";
+let num = Number(num1);
+console.log(typeof num);  // number
+
+// number to string
+let num2 = 55;
+let str = String(num2);
+console.log(typeof str);  // string
+
+// string to boolean
+let str1 = "hello";
+let bool = Boolean(str1);
+console.log(typeof bool);  // boolean
