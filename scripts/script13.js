78164663ea3258ee97e7b3f7541b648d05ed3333
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 14:42:18 +0530

    solution 13

---
+++ scripts/script13.js
@@ -0,0 +1,20 @@
+let val1 = "name";
+console.log(typeof val1); //output : string
+
+let val2 = 10;
+console.log(typeof val2); //output : number
+
+let val3 = null;  
+console.log(typeof val3);  //output : object
+
+let val4 = undefined;
+console.log(typeof val4); //output : undefined
+
+let val5 = true;
+console.log(typeof val5); //output : boolean
+
+let val6 = Symbol("id");
+console.log(typeof val6); //output : symbol
+
+let val7 = BigInt("1234567890123456789012345678901234567890");
+console.log(typeof val7); //output : bigint
