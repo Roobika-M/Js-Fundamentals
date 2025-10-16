9ebaf28dcaabe723f2eaebca4474af2aaa90f7a1
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 16:58:15 +0530

    solution 18

---
+++ scripts/script18.js
@@ -0,0 +1,11 @@
+// in case of prompt, its either a value (which is string) or null
+
+let num = prompt("enter a number");
+alert(num);
+// in the above code when user enters a value and click ok, the number is displayed
+// if user clicks cancel, null is displayed
+
+let con = confirm("are you sure?");
+alert(con);
+// in the above code when user clicks ok, true is displayed
+// if user clicks cancel, false is displayed
