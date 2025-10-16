2d30dea88a3eeb272925524f03bd008d49f63a9a
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-16 19:09:00 +0530

    solution 24

---
+++ scripts/script24.js
@@ -0,0 +1,13 @@
+let num = 5;
+
+num--;
+console.log(num); // Outputs: 4
+
+num++;
+console.log(num); // Outputs: 5
+
+--num;
+console.log(num); // Outputs: 4
+
+++num;
+console.log(num); // Outputs: 5
