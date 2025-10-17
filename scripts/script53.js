b8d35934ec549ba5903c4fe5ee1e663b4eef91f6
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-17 08:48:18 +0530

    solution 53

---
+++ scripts/script53.js
@@ -0,0 +1,3 @@
+console.log(0.1+0.2===0.3); //gives false
+
+// this happens because js stores number in binary format. in that case, the addition of binary numbers of 0.1 and 0.2 is slightly greater than 0.3. so it results in false
