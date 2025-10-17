8ea18f5094daf5312a2221ac4e88a1cf9815c97c
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-17 08:56:38 +0530

    solution 54

---
+++ scripts/script54.js
@@ -0,0 +1,10 @@
+// toString() is used to convert any value to string
+// valueOf() is used to convert the value to number
+
+let d = new Date();
+console.log(d.toString());
+console.log(d.valueOf());
+
+// output
+// Fri Oct 17 2025 08:55:38 GMT+0530 (India Standard Time)
+// VM45:3 1760671538255
