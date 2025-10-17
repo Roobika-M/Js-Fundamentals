eb5972ef7fe7ff03f8128f11bd0dd60b133f6eaf
Roobika <153609390+Roobika-M@users.noreply.github.com>
2025-10-17 09:13:38 +0530

    solution 57

---
+++ scripts/script57.js
@@ -0,0 +1,8 @@
+// in operator
+
+let user = {name : "peter", age : 44};
+console.log("name" in user)     //output : true, since the key is present in user
+
+let arr = [2,3,4,5];
+console.log(1 in arr) //output : true, since index exists
+
